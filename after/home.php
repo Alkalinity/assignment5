9f33fc12d197942b3b92a87a04aae04daca2a03b
Sam Pakulski <spakulsk@uvm.edu>
2015-03-17 13:39:16 -0400

    Added layout for my site, subject to change
    Added in the header nav and footer, and setup articles and asides for
    the bulk of the content that's going to be on the main page

--- after/home.php
+++ after/home.php
@@ -1,17 +1,71 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
+<html lang="en">
+    <!-- __ _ _ _ __| |_ (_)__ _____ 
+        / _` | '_/ _| ' \| |\ V / -_)
+        \__,_|_| \__|_||_|_| \_/\___| -->
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <title>Internet Archive: Digital Library of Free Books, Movies, Music & Wayback Machine</title>
+        
+        <meta name="DESCRIPTION" content="Internet Archive is a non-profit digital library offering free universal access to books, movies & music, as well as 456 billion archived web pages."/>
+        <link href="style.css" rel="stylesheet" type="text/css"/>
+        <link rel="SHORTCUT ICON" href="/images/glogo.jpg"/>
     </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
-</html>
+   <!--########### TITLE SECTION #########-->
+   
+    <h1>The Internet Archive</h1>
+    <h2>Universal Access To All Knowledge</h2>
+    
+   <!--############### NAVIGATION SECTION ###############-->
+    <nav>
+        <ol>
+            <li>Home</li>
+            <li><a href="https://archive.org/web/">Web</a></li>
+            <li><a href="https://archive.org/details/movies">Video</a></li>
+            <li><a href="https://archive.org/details/texts">Text</a></li>
+            <li><a href="https://archive.org/details/audio">Audio</a></li>
+            <li><a href="https://archive.org/details/etree">Live Music</a></li>
+            <li><a href="https://archive.org/details/software">Software</a></li>
+        </ol>
+    </nav>
+   
+   
+   <!-- ############### MAIN CONTENT SECTION ###############-->
+   <article id="boxes">
+       <aside class="web">
+           <p>Content</p>
+       </aside>
+       
+       <aside class="video">
+           <p>Content</p>
+       </aside>
+       
+       <aside class="text">
+           <p>Content</p>
+       </aside>
+       
+       <aside class="audio">
+           <p>Content</p>
+       </aside>
+       
+       <aside class="music">
+           <p>Content</p>
+       </aside>
+       
+       <aside class="software">
+           <p>Content</p>
+       </aside>
+   
+   </article>
+   
+   <article class="anouncements">
+       <p>Content</p>
+   </article>
+   
+   <article class="about">
+       <p>Content</p>
+   </article>
+   
+   <footer>
+       <p>Samuel Pakulski</p>
+   </footer>
+    

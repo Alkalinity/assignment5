643ddf1bf1b6ec939229aa19959b1b891144d0e3
Sam Pakulski <spakulsk@uvm.edu>
2015-04-06 15:26:42 -0400

    Big Update
    Filled in Content and fleshed out site, also changed color scheme to fit
    the feel of the site

--- after/home.php
+++ after/home.php
@@ -5,17 +5,17 @@
         \__,_|_| \__|_||_|_| \_/\___| -->
     <head>
         <title>Internet Archive: Digital Library of Free Books, Movies, Music & Wayback Machine</title>
-        
+
         <meta name="DESCRIPTION" content="Internet Archive is a non-profit digital library offering free universal access to books, movies & music, as well as 456 billion archived web pages."/>
         <link href="style.css" rel="stylesheet" type="text/css"/>
         <link rel="SHORTCUT ICON" href="/images/glogo.jpg"/>
     </head>
-   <!--########### TITLE SECTION #########-->
-   <img class="Logo" src="images/logo.png" alt="logo" title="Internet Archive">
+    <!--########### TITLE SECTION #########-->
+    <img class="Logo" src="images/logo.png" alt="logo" title="Internet Archive">
     <h1 id="mainTitle">The Internet Archive</h1>
     <h2 id="mainHeading">Universal Access To All Knowledge</h2>
-    
-   <!--############### NAVIGATION SECTION ###############-->
+
+    <!--############### NAVIGATION SECTION ###############-->
     <nav>
         <ol>
             <li class="activePage">Home</li>
@@ -25,90 +25,142 @@
             <li><a href="https://archive.org/details/audio">Audio</a></li>
             <li><a href="https://archive.org/details/etree">Live Music</a></li>
             <li><a href="https://archive.org/details/software">Software</a></li>
+            <li><a href="home.php"><img src="images/search.png" class="search"></a></li>
+            <li class="rss"><a href=""><img src="images/rss.png"></a></li>
             <li class="gdlogo"><a href=""><img src="images/gdlogo.gif" alt="Gd logo" title="Grateful Dead Logo"></a></li>
+            <li class="donate"><a href=""><img src="images/donate.gif"></a></li>
         </ol>
     </nav>
-   
-   
-   <!-- ############### MAIN CONTENT SECTION ###############-->
-   <article id="boxes">
-       <aside class="web">
-           <h1><a href="https://archive.org/web/">Web</a></h1>
-           <p>456 Billion Webpages</p>
-           <h4>Quick Links</h4>
-           <ol>
-               <li>Tools</li>
-               <li>Subscription Service</li>
-               <li>Page Saver</li>
-           </ol>
-       </aside>
-       
-       <aside class="video">
-           <h1><a href="https://archive.org/details/movies">Video</a></h1>
-           <p>1,923,084 Movies and Videos</p>
-           <h4>Quick Links</h4>
-           <ol>
-               <li>Animation And Cartoons</li>
-               <li>Full Length Movies</li>
-               <li>Vlogs</li>
-           </ol>
-       </aside>
-       
-       <aside class="text">
-           <h1><a href="https://archive.org/details/texts">Text</a></h1>
-           <p>7,801,999 Texts and Books</p>
-           <h4>Quick Links</h4>
+
+
+    <!-- ############### MAIN CONTENT SECTION ###############-->
+    <article id="boxes">
+        <aside class="web">
+            <h1><a href="https://archive.org/web/">Web Archive</a></h1>
+            <p>456 Billion Webpages</p>
+            <h4>Quick Links</h4>
             <ol>
-               <li>Borrow A Book</li>
-               <li>Contribute A Book</li>
-               <li>Digitalize</li>
-           </ol>          
-       </aside>
-       
-       <aside class="audio">
-           <h1><a href="https://archive.org/details/audio">Audio</a></h1>
-           <p>2,378,993 Recordings</p>
-           <h4>Quick Links</h4>
-           <ol>
-               <li>Audiobooks and Poetry</li>
-               <li>Podcasts</li>
-               <li>Contribute</li>
-           </ol>           
-       </aside>
-       
-       <aside class="music">
-           <h1><a href="https://archive.org/details/etree">Live Music</a></h1>
-           <p>139,054 Concerts</p>
-           <h4>Quick Links</h4>
-           <ol>
-               <li>Grateful Dead</li>
-               <li>moe.</li>
-               <li>List of Artists</li>
-           </ol>           
-       </aside>
-       
-       <aside class="software">
-           <h1><a href="https://archive.org/details/software">Software</a></h1>
-           <p>96,049 Pieces of Software</p>
-           <h4>Quick Links</h4>
-           <ol>
-               <li>Emulation Center</li>
-               <li>CD Archive</li>
-               <li>Classic PC Games</li>
-           </ol>           
-       </aside>
-   
-   </article>
-   
-   <article class="anouncements">
-       <p>Content</p>
-   </article>
-   
-   <article class="about">
-       <p>Content</p>
-   </article>
-   
-   <footer>
-       <p>Samuel Pakulski</p>
-   </footer>
-    
+                <li><a href="https://archive.org/web/">Tools</a></li>
+                <li><a href="https://archive.org/web/">Subscription Service</a></li>
+                <li><a href="https://archive.org/web/">Page Saver</a></li>
+            </ol>
+            <h4>Most Recent Post</h4>
+            <p><a href="home.php">Click for Most Recent Web Page Saved!</a></p>
+        </aside>
+
+        <aside class="video">
+            <h1><a href="https://archive.org/details/movies">Video Archive</a></h1>
+            <p>1,923,084 Movies and Videos</p>
+            <h4>Quick Links</h4>
+            <ol>
+                <li><a href="https://archive.org/details/animationandcartoons">Animation And Cartoons</a></li>
+                <li><a href="https://archive.org/details/moviesandfilms">Full Length Movies</a></li>
+                <li><a href="https://archive.org/details/vlogs">Vlogs</a></li>
+            </ol>
+            <h4>Most Recent Post</h4>
+            <p><a href="">PC Video For Arabic</a></p>
+            <img src="" class="videoImage">
+        </aside>
+
+        <aside class="text">
+            <h1><a href="https://archive.org/details/texts">Text Archive</a></h1>
+            <p>7,801,999 Texts and Books</p>
+            <h4>Quick Links</h4>
+            <ol>
+                <li><a href="https://openlibrary.org/borrow">Borrow A Book</a></li>
+                <li><a href="https://archive.org/upload/">Contribute A Book</a></li>
+                <li><a href="http://archive.org/scanning">Digitalize</a></li>
+            </ol>       
+            <h4>Most Recent Post</h4>
+            <p><a href="">Of Gardens, An Essay</a></p>
+            <img src="" class="textImage">
+        </aside>
+
+        <aside class="audio">
+            <h1><a href="https://archive.org/details/audio">Audio Archive</a></h1>
+            <p>2,378,993 Recordings</p>
+            <h4>Quick Links</h4>
+            <ol>
+                <li><a href="https://archive.org/details/audio_bookspoetry">Audiobooks and Poetry</a></li>
+                <li><a href="https://archive.org/details/audio_podcast">Podcasts</a></li>
+                <li><a href="https://archive.org/upload/">Contribute</a></li>
+            </ol>           
+            <h4>Most Recent Post</h4>
+            <p><a href="">Minus, Castofon</a></p>
+            <img src="" class="audioImage">            
+        </aside>
+
+        <aside class="music">
+            <h1><a href="https://archive.org/details/etree">Live Music Archive</a></h1>
+            <p>139,054 Concerts</p>
+            <h4>Quick Links</h4>
+            <ol>
+                <li><a href="https://archive.org/details/GratefulDead">Grateful Dead</a></li>
+                <li><a href="https://archive.org/details/moe">moe.</a></li>
+                <li><a href="https://archive.org/browse.php?collection=etree&field=creator">List of Artists</a></li>
+            </ol> 
+            <h4>Most Recent Post</h4>
+            <p><a href="">Grateful Dead Live at Nashville Municipal...</a></p>
+            <img src="" class="liveImage">
+        </aside>
+
+        <aside class="software">
+            <h1><a href="https://archive.org/details/software">Software Archive</a></h1>
+            <p>96,049 Pieces of Software</p>
+            <h4>Quick Links</h4>
+            <ol>
+                <li><a href="https://archive.org/details/tosec">Emulation Center</a></li>
+                <li><a href="https://archive.org/details/cdbbsarchive">CD Archive</a></li>
+                <li><a href="https://archive.org/details/classicpcgames">Classic PC Games</a></li>
+            </ol> 
+            <h4>Most Recent Post</h4>
+            <p><a href="">The Oregan Trail Deluxe</a></p>
+            <img src="" class="softwareImage">            
+        </aside>
+
+    </article>
+
+    <article class="about">
+        <h1>About The Internet Archive</h1>
+        <p>The Internet Archive, a 501(c)(3) non-profit, is building a digital library of Internet sites and other cultural artifacts in digital form. Like a paper library, we provide free access to researchers, historians, scholars, the print disabled, and the general public.</p>
+        <ol>
+            <li><a href="">Donate</a></li>
+            <li><a href="">Store</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">FAQ</a></li>
+            <li><a href="">Jobs</a></li>
+            <li><a href="">Volunteer</a></li>
+        </ol>
+    </article>
+
+    <article class="anouncements">
+        <h1>Announcements</h1>
+        <ul>
+            <li><a href="https://blog.archive.org/2015/03/19/political-ads-win-over-news-45-to-1-in-philly-tv-news-2014/">Political Ads Win Over News 45 to 1 in Philly TV News 2014</a></li>
+            <li><a href="https://blog.archive.org/2015/02/21/you-are-invited-to-a-party-victory-for-the-net/">You are Invited to a Party: Victory for the Net</a></li>
+            <li><a href="https://blog.archive.org/2015/02/11/locking-the-web-open-a-call-for-a-distributed-web/">Locking the Web Open, a Call for a Distributed Web</a></li>
+        </ul>
+    </article>
+
+    <article class="institution">
+        <h1>Institutional Support</h1>
+        <ul>
+            <li><a href="https://archive.org/about/credits.php#alexa">Alexa Internet</a></li>
+            <li><a href="https://archive.org/about/credits.php#HP">HP Computer</a></li>
+            <li><a href="https://archive.org/about/credits.php#kahle">The Kahle/Austin Foundation</a></li>
+        </ul>
+        <ul>
+            <li><a href="https://archive.org/about/credits.php#prelinger">Prelinger Archives</a></li>
+            <li><a href="https://archive.org/about/credits.php#nsf">National Science Foundation</a></li>
+            <li><a href="https://archive.org/about/credits.php#loc">Library of Congress</a></li>
+        </ul>
+        <ul>
+            <li><a href="https://archive.org/about/credits.php#lt">LizardTech</a></li>
+            <li><a href="http://www.sloan.org/">Sloan Foundation</a></li>
+            <li><a href="https://archive.org/about/contributors.php">Individual Contributors</a></li>
+        </ul>
+    </article>
+
+    <footer>
+        <p>Web Designed By: Samuel Pakulski  -  <a href="https://archive.org/about/terms.php">Terms Of Use</a>  -  Last Updated: 4/6/2015</p>
+    </footer>
